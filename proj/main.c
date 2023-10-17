3da110dd28b89ff7f512d78c95a272c9ab8b220a
phantom <matheusbaeck@gmail.com>
2023-10-17 18:16:05 +0200

    phantom

--- proj/main.c
+++ proj/main.c
@@ -215,10 +215,26 @@ int	init(t_data *data)
 	return (0);
 }
 
-int	main(void)
+int	get_entry(int argc, char **argv)
+{
+	if (argc < 2)
+		return (1);
+	else if (argc == 2)
+	{
+		if (!(ft_strncmp(argv[1], "Mandelbrot", 11) || ft_strncmp(argv[1], "Mandelbrot3", 12) || ft_strncmp(argv[1], "Julia", 6)))
+			return (printf("fractal not found, try one of the following \n\nMandelbrot\nMandelbrot3\n Julia <param1> <param2>\n"), 1);
+	}
+	else if (argc != 4)
+		return (printf("wrong entry\n\n"), 1);
+	return (0);
+}
+
+int	main(int argc, char **argv)
 {
 	t_data	data;
 
+	if (!get_entry(arc, argv))
+		return (0);
 	data.mlx_ptr = mlx_init();
 	if (data.mlx_ptr == NULL)
 		return (MLX_ERROR);
@@ -230,7 +246,8 @@ int	main(void)
 	data.img.addr = mlx_get_data_addr(data.img.mlx_img, &data.img.bpp,
 			&data.img.line_len, &data.img.endian);
 	set_cartesian_plan(&data.cart, (t_cartesian){-2, 2, -3, 3, 4, 6});
-	set_freactol(&data.frctl, (t_fractol){{0, 0}, {0, 0}, 3, 0.1, 0.1, 1, 0, 5, 80, 80, 0, 2});
+	if (argc != 4)
+		set_freactol(&data.frctl, (t_fractol){{0, 0}, {0, 0}, 3, 0.1, 0.1, 1, 0, 5, 80, 80, 0, 2});
 	mlx_loop_hook(data.mlx_ptr, &render, &data);
 	mlx_hook(data.win_ptr, 2, 0, &handle_keypress, &data);
 	mlx_hook(data.win_ptr, 3, 0, &handle_keyrelease, &data);
